2a0adfcde22b4ba5feae9156a0a9f77ae3a26978
AndreiBelich <balichandrey89@gmail.com>
2021-04-27 22:28:53 +0300

    chnage code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,71 +1,100 @@
 "use strict";
 
-const form = document.querySelector("#root-form");
-const showButton = document.querySelector(".showData");
-showButton.addEventListener("click", (event) => {
+const makeId = (value = 0) => () => ++value;
+
+const form = document.querySelector(".main-form");
+const showButton = document.querySelector("#showButton");
+const ul = document.querySelector("#root");
+const userValues = [];
+const getNextId = makeId();
+
+form.addEventListener("submit", submitHandler);
+
+showButton.addEventListener("click", () => {
   fillUnorderedList(userValues);
+  checkData();
 });
 
-function getId(value = 0){
-  return () => {
-    value++;
-    return value;
+function submitHandler(event){
+  event.preventDefault();
+  const {
+    target,
+    target: {
+      elements: {userInput}
+    }
+  } = event;
+  if(userInput.value){
+    userValues.push({
+      id: getNextId(),
+      value: userInput.value.trim()
+    });
   }
+  target.reset();
 }
 
-const addId = getId();
-
-function createListItem(value){
+function createListItem(id, value){
   const li = document.createElement("li");
-  li.dataset.id = addId();
+  li.classList.add("listItem");
+  li.dataset.id = id;
+
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("flexContainer");
+
   const div = document.createElement("div");
+  div.classList.add("userText")
   div.innerText = value;
   const button = document.createElement("button");
-  button.innerText = "reset";
+  button.classList.add("deleteButton");
+  button.innerText = "delete";
+  wrapper.append(div, button);
+
   button.addEventListener("click", ()=> {
     removeItem(li.dataset.id);
   });
-  li.append(div, button);
+
+  li.append(wrapper);
   return li;
 }
 
 function removeItem(id){
   const items = [...document.querySelectorAll("li")];
   const findItem = items.find((item) => item.dataset.id === id);
-  const ul = document.querySelector(".root");
+  let index = -1;
+  for(let i = 0; i < userValues.length; i++){
+    if(userValues[i].id === +findItem.dataset.id){
+      index = i;
+      break;
+    }
+  }
   if(findItem){
     ul.removeChild(findItem);
+    userValues.splice(index, 1);
+  }
+  checkData(); 
+}
+
+function checkData(){
+  if(userValues.length){
+    ul.classList.add("rootBorder");
+  }else{
+    ul.classList.remove("rootBorder");
   }
 }
 
 function fillUnorderedList(data){
-  const ul = document.querySelector(".root");
-  clearUnorderedList(ul);
+  const listItems = [...document.querySelectorAll("#root > li")];
+  const idList = [];
+  for(const item of listItems){
+    idList.push(+item.dataset.id);
+  }
   for(const item of data){
-    const li = createListItem(item);
-    ul.append(li);
-  }
-}
-
-function clearUnorderedList(tag){
-  const items = [...tag.querySelectorAll("li")];
-  for(const item of items){
-    tag.removeChild(item);
-  }
-}
-
-const userValues = [];
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const {
-    target,
-    target: {
-      elements: {user}
+    const {id, value} = item;
+    if(!idList.includes(id))
+    {
+      const li = createListItem(id, value);
+      ul.append(li);
     }
-  } = e;
-  if(user.value){
-    userValues.push(user.value);
   }
-  target.reset();
-});
+}
+
+
